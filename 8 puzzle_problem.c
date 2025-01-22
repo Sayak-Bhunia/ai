307f2915ad001ae2247b0dae52d697ef49542f2c
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-01-22 13:22:34 +0530

    Update 8 puzzle_problem.c

--- 8 puzzle_problem.c
+++ 8 puzzle_problem.c
@@ -3,24 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define N 3
+#define n 3
 
 struct Node {
     struct Node* parent;
-    int mat[N][N];
-    int x, y;
-    int cost;
-    int level;
+    int mat[n][n], x, y, cost, level;
 };
 
-void printMatrix(int mat[N][N]) {
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) printf("%d ", mat[i][j]);
+void printMatrix(int mat[n][n]) {
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) printf("%d ", mat[i][j]);
         printf("\n");
     }
 }
 
-struct Node* newNode(int mat[N][N], int x, int y, int newX, int newY, int level, struct Node* parent) {
+struct Node* newNode(int mat[n][n], int x, int y, int newX, int newY, int level, struct Node* parent) {
     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
     node->parent = parent;
     memcpy(node->mat, mat, sizeof(node->mat));
@@ -34,19 +31,19 @@ struct Node* newNode(int mat[N][N], int x, int y, int newX, int newY, int level,
     return node;
 }
 
-int row[] = { 1, 0, -1, 0 };
-int col[] = { 0, -1, 0, 1 };
+int row[] = {1, 0, -1, 0};
+int col[] = {0, -1, 0, 1};
 
-int calculateCost(int initial[N][N], int final[N][N]) {
+int calc(int a[n][n], int b[n][n]) {
     int c = 0;
-    for(int i=0;i<N;i++)
-        for(int j=0;j<N;j++)
-            if(initial[i][j] && initial[i][j] != final[i][j]) c++;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            if(a[i][j] && a[i][j] != b[i][j]) c++;
     return c;
 }
 
 int isSafe(int x, int y) {
-    return (x>=0 && x<N && y>=0 && y<N);
+    return (x>=0 && x<n && y>=0 && y<n);
 }
 
 void printPath(struct Node* root) {
@@ -62,38 +59,37 @@ int comp(const void* lhs, const void* rhs) {
     return (l->cost + l->level) - (r->cost + r->level);
 }
 
-void solve(int initial[N][N], int x, int y, int final[N][N]) {
+void solve(int a[n][n], int x, int y, int b[n][n]) {
     struct Node* pq[100000];
-    int n = 0;
-    struct Node* root = newNode(initial, x, y, x, y, 0, NULL);
-    root->cost = calculateCost(initial, final);
-    pq[n++] = root;
-    while(n>0) {
-        qsort(pq, n, sizeof(struct Node*), comp);
-        struct Node* min = pq[0];
-        for(int i=1;i<n;i++) pq[i-1] = pq[i];
-        n--;
-        if(min->cost == 0) {
-            printPath(min);
+    int sz = 0;
+    struct Node* root = newNode(a, x, y, x, y, 0, NULL);
+    root->cost = calc(a, b);
+    pq[sz++] = root;
+    while(sz>0) {
+        qsort(pq, sz, sizeof(struct Node*), comp);
+        struct Node* mn = pq[0];
+        for(int i=1;i<sz;i++) pq[i-1] = pq[i];
+        sz--;
+        if(mn->cost == 0) {
+            printPath(mn);
             return;
         }
         for(int i=0;i<4;i++) {
-            if(isSafe(min->x + row[i], min->y + col[i])) {
+            if(isSafe(mn->x + row[i], mn->y + col[i])) {
                 struct Node* child = newNode(
-                    min->mat, min->x, min->y,
-                    min->x + row[i], min->y + col[i],
-                    min->level + 1, min);
-                child->cost = calculateCost(child->mat, final);
-                pq[n++] = child;
+                    mn->mat, mn->x, mn->y,
+                    mn->x + row[i], mn->y + col[i],
+                    mn->level + 1, mn);
+                child->cost = calc(child->mat, b);
+                pq[sz++] = child;
             }
         }
     }
 }
 
 int main() {
-    int initial[N][N] = {{1, 2, 3}, {5, 6, 0}, {7, 8, 4}};
-    int final[N][N] = {{1, 2, 3}, {5, 8, 6}, {0, 7, 4}};
+    int a[n][n] = {{1, 2, 3}, {5, 6, 0}, {7, 8, 4}};
+    int b[n][n] = {{1, 2, 3}, {5, 8, 6}, {0, 7, 4}};
     int x = 1, y = 2;
-    solve(initial, x, y, final);
-    return 0;
+    solve(a, x, y, b);
 }
